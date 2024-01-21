c26cab852e04af4baa2cc2c35e3aeb8a2aec9bd3
Ferenc Kozak <kozakf@gmail.com>
2024-01-22 00:10:35 +0100

    Create oauth2 login and token acquisition

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,71 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {OAuthService} from "angular-oauth2-oidc";
+import {authCodeFlowConfig} from "./auth.config";
+import {LocalStorageService} from './storage.service';
+import {NgIf} from "@angular/common";
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'oauth';
+  title = 'OAuth2 Azure AD test';
+
+  constructor(private oauthService: OAuthService,
+              private storage: LocalStorageService,
+              private http: HttpClient) {
+    this.oauthService.configure(authCodeFlowConfig);
+    this.oauthService.loadDiscoveryDocumentAndTryLogin().then(value => {
+      console.log(this.oauthService.getAccessToken())
+      if (this.oauthService.hasValidAccessToken()) {
+        this.http.get<AuthResponse>('http://localhost:8080/api/auth/v1/oauth2/token')
+          .subscribe({
+            next: (data) => {
+              storage.set("username", data.username)
+              storage.set("token", data.token)
+            },
+            error: (error) => {
+              storage.remove("username")
+              storage.remove("token")
+            }
+          });
+      }
+    });
+  }
+
+  public login() {
+    this.oauthService.initCodeFlow()
+  }
+
+  public logout() {
+    this.storage.remove("username")
+    this.storage.remove("token")
+    this.oauthService.logOut(true);
+    return false;
+  }
+
+  public get authorized() {
+    return this.storage.get("token") !== null
+  }
+
+  public get userName() {
+    if(this.authorized)
+      return this.storage.get("username")
+    return ""
+  }
+}
+
+export interface AuthResponse {
+  id: number;
+  lastName: string;
+  firstName: string;
+  username: string;
+  email: string;
+  token: string;
+  modules: any;
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,21 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {ApplicationConfig, importProvidersFrom} from '@angular/core';
+import {provideRouter} from '@angular/router';
+import {HttpClientModule} from "@angular/common/http";
+import {OAuthModule} from 'angular-oauth2-oidc';
 
-import { routes } from './app.routes';
+import {routes} from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    importProvidersFrom(HttpClientModule),
+    importProvidersFrom(
+      OAuthModule.forRoot({
+        resourceServer: {
+          allowedUrls: ["http://localhost:8080/api/auth/v1/oauth2"],
+          sendAccessToken: true,
+        },
+      })
+    )
+  ]
 };
